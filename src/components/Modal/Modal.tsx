4b9e220473437daf09933eba151b6315f876350a
Angus Chang <angus@heysparkbox.com>
2022-08-11 21:18:39 -0400

    feat: created basic structure for modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { createPortal } from 'react-dom';
+import Button from '../Button';
+import './Modal.css';
 
-interface ModalProps {
-
+export interface ModalProps {
+  title: string,
+  containerClassName?: string,
+  contentClassName?: string,
 }
 
-const Modal: React.FC<ModalProps> = ({}) => {
-    return (
-      <></>
-    );
+const Modal: React.FC<ModalProps> = ({ children, title, containerClassName, contentClassName }) => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const modalContainer = () => {
+    return createPortal(
+      <aside className={ `${containerClassName} container` } role="dialog" aria-modal="true">
+        <div className={ `${contentClassName} content` }>
+          { children }
+          <Button onClick={() => setIsModalVisible(false)}>Close</Button>
+        </div>
+      </aside>,
+      document.body
+    )
+  }
+
+  return (
+    <React.Fragment>
+      <Button onClick={() => setIsModalVisible(true)}>{ title }</Button>
+      {isModalVisible && modalContainer()}
+    </React.Fragment>
+  )
 }
 
 export default Modal;
