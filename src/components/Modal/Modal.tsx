ff99b998c93677771b911404b39c4539ca3a6fc0
Angus Chang <angus@heysparkbox.com>
2022-08-12 22:28:51 -0400

    feat: add event listener for escape key to close modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { createPortal } from 'react-dom';
 import Button from '../Button';
 import './Modal.css';
@@ -12,10 +12,28 @@ export interface ModalProps {
 const Modal: React.FC<ModalProps> = ({ children, title, containerClassName, contentClassName }) => {
   const [isModalVisible, setIsModalVisible] = useState(false);
 
+  useEffect(() => {
+    function keyListener(event: KeyboardEvent) {
+      const key = event.key || event.keyCode;
+      if(key === 'Escape' || key === 'Esc' || key === 27) {
+        setIsModalVisible(false);
+      }
+    }
+
+    document.addEventListener("keydown", keyListener);
+    return () => document.removeEventListener("keydown", keyListener);
+  })
+
   const modalContainer = () => {
     return createPortal(
-      <aside className={ `${containerClassName} container` } role="dialog" aria-modal="true">
-        <div className={ `${contentClassName} content` }>
+      <aside 
+        className={ `${containerClassName} container` } 
+        role="dialog" 
+        aria-modal="true"
+        >
+        <div 
+          className={ `${contentClassName} content` }
+          onClick={() => null }>
           { children }
           <Button onClick={() => setIsModalVisible(false)}>Close</Button>
         </div>
