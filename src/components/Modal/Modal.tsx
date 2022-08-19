8825cc9316bfe1e9ccbcbe8bb815753e42a0b7de
Angus Chang <angus@heysparkbox.com>
2022-08-18 20:44:52 -0400

    feat: create modal functionality and change class names

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { RefObject, useEffect, useState } from 'react';
 import { createPortal } from 'react-dom';
 import Button from '../Button';
 import './Modal.css';
@@ -15,28 +15,73 @@ const Modal: React.FC<ModalProps> = ({ children, title, containerClassName, cont
   useEffect(() => {
     function keyListener(event: KeyboardEvent) {
       const key = event.key || event.keyCode;
-      if(key === 'Escape' || key === 'Esc' || key === 27) {
-        setIsModalVisible(false);
-      }
+      const listener = keyListenerMap.get(key);
+      return listener && listener(event);
     }
+
+    setFirstFocusElement();
 
     document.addEventListener("keydown", keyListener);
     return () => document.removeEventListener("keydown", keyListener);
-  })
+  },)
+
+  const modalRef: RefObject<HTMLDivElement> = React.createRef<HTMLDivElement>();
+
+  const setFirstFocusElement = () => {
+    if(modalRef.current) {
+      const focusableModalElements = modalRef.current.querySelectorAll<HTMLElement>(
+        'a[href], button, textarea, input[type="text"], input[type="radio"], input[type="checkbox"], select'
+      );
+        
+      // define the first and last elements of our modal:
+      focusableModalElements[0].focus();
+    }
+  }
+
+  const handleTabKey = (event: KeyboardEvent) => {
+    if(modalRef.current) {
+        const focusableModalElements = modalRef.current.querySelectorAll<HTMLElement>(
+          'a[href], button, textarea, input[type="text"], input[type="radio"], input[type="checkbox"], select'
+        );
+        
+        // define the first and last elements of our modal:
+        const firstElement = focusableModalElements[0];
+        const lastElement = focusableModalElements[focusableModalElements.length - 1];
+          
+        if (!event.shiftKey && document.activeElement === lastElement) {
+          firstElement.focus();
+          return event.preventDefault();
+        } else if (event.shiftKey && document.activeElement === firstElement) {
+          lastElement.focus();
+          return event.preventDefault();
+        } 
+      }
+  };
+
+
+
+  const keyListenerMap = new Map<string | number, (...args: any[]) => void>([
+    ['Tab' || 9, handleTabKey],
+    [27 || 'Escape' || 'Esc', () => setIsModalVisible(false)]
+  ]);
 
   const modalContainer = () => {
     return createPortal(
       <aside 
-        className={ `${containerClassName} container` } 
+        className={ `${containerClassName} modal-container` } 
         role="dialog" 
         aria-modal="true"
         >
         <div 
-          className={ `${contentClassName} content` }
-          onClick={() => null }>
+          className={ `${contentClassName} modal-content` }
+          onClick={() => null }
+          ref={modalRef}
+          >
           { children }
-          <Button onClick={() => setIsModalVisible(false)}>Close</Button>
-        </div>
+          <Button 
+            onClick={() => setIsModalVisible(false)}
+          >Close</Button>
+        </div>  
       </aside>,
       document.body
     )
