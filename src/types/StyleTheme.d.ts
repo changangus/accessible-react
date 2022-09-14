d29b3c84ac3cfbd3ebc6574efbcec6cb46a2f64a
Angus Chang <angus@heysparkbox.com>
2022-09-14 11:31:40 -0400

    feat: create StyleTheme type

---
+++ src/types/StyleTheme.d.ts
@@ -0,0 +1,14 @@
+type StyleTheme = {
+  colors?: {
+    primary?: string;
+    secondary?: string;
+  },
+  fonts?: {
+    headingOne?: string;
+    headingTwo?: string;
+    headingThree?: string;
+    body?: string;
+  }
+}
+
+export default StyleTheme;
