62d1870833ad93411141198fd2f3a13a32fa8a4c
Angus Chang <angus@heysparkbox.com>
2022-09-21 20:58:30 -0400

    test: create tests for button component

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,40 +1,62 @@
-import '@testing-library/jest-dom'
+import "@testing-library/jest-dom";
 import React from "react";
 import type { RenderResult } from "@testing-library/react";
 import { render, screen } from "@testing-library/react";
 import Button from "./Button";
+import { ThemeProvider } from "react-jss";
+import createTheme from "../../utils/createTheme";
 
 describe("Button", () => {
   const clickFunction = jest.fn();
-  const setup = (): RenderResult => {
+  const theme = createTheme();
+  const setup = (styleType?: "contained" | "outlined"): RenderResult => {
     return render(
-    <Button 
-      ariaLabel="submit form"
-      onClick={clickFunction}
-      type="submit"
-    >
-      Submit
-    </Button>)
+      <ThemeProvider theme={theme} >
+        <Button ariaLabel="submit form" onClick={clickFunction} type="submit" styleType={styleType}>
+          Submit
+        </Button>
+        )
+      </ThemeProvider>
+    );
   };
 
-  beforeEach(() => setup());
-
-
   test("renders the Button component with children", () => {
-    const button = screen.getByRole('button');
+    setup();
+    const button = screen.getByRole("button");
     expect(button).toBeInTheDocument();
-    expect(button).toHaveTextContent('Submit');
+    expect(button).toHaveTextContent("Submit");
   });
 
   test("passes in props correctly", () => {
-    const button = screen.getByRole('button')
-    expect(button).toHaveAttribute('aria-label', 'submit form');
-    expect(button).toHaveAttribute('type', 'submit');
+    setup();
+    const button = screen.getByRole("button");
+    expect(button).toHaveAttribute("aria-label", "submit form");
+    expect(button).toHaveAttribute("type", "submit");
   });
 
-  test('Runs onClick function prop when button is clicked', () => {
-    const button = screen.getByRole('button')
+  test("Runs onClick function prop when button is clicked", () => {
+    setup();
+    const button = screen.getByRole("button");
     button.click();
     expect(clickFunction).toHaveBeenCalled();
-  })
-});
+  });
+  
+  // we only need to check a single different style between the two styles
+  test("Renders the button with the correct styles when nothing passed in", () => {
+    setup();
+    const button = screen.getByRole("button");
+    expect(button).toHaveStyle(`background-color: ${theme.colors.primary}`);
+  });
+
+  test("Runs containedStyles when styleType is contained", () => {
+    setup("contained");
+    const button = screen.getByRole("button");
+    expect(button).toHaveStyle(`background-color: ${theme.colors.primary}`);
+  });
+
+  test("Runs outlineStyles when styleType is outlined", () => {
+    setup("outlined");
+    const button = screen.getByRole("button");
+    expect(button).toHaveStyle(`background-color: transparent`);
+  }); 
+}); 
